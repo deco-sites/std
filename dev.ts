7aaa9208cf8242eecb9e231943fd63fa7a25edc7
Marcos Candeia <marrcooos@gmail.com>
2023-03-16 18:25:45 -0300

    Use new live version
    Signed-off-by: Marcos Candeia <marrcooos@gmail.com>

--- dev.ts
+++ dev.ts
@@ -1,5 +1,5 @@
 #!/usr/bin/env -S deno run -A --watch=static/
 import dev from "$live/dev.ts";
-import liveManifest from "$live/fresh.gen.ts";
+import liveManifest from "$live/live.gen.ts";
 
-await dev(import.meta.url, "./main.ts", { imports: { "$live": liveManifest } });
+await dev(import.meta.url, "./main.ts", { imports: [liveManifest] });
