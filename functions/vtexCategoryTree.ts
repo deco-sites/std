16e8f7259751a3d9354e777f8fdec70ab0ab59b0
lfroes <nikowluiz@gmail.com>
2023-03-17 10:44:24 -0300

    Fix: added improvements to the code

--- functions/vtexCategoryTree.ts
+++ functions/vtexCategoryTree.ts
@@ -4,18 +4,8 @@ import { ConfigVTEX, createClient } from "../commerce/vtex/client.ts";
 import { withISFallback } from "../commerce/vtex/withISFallback.ts";
 
 export interface Props {
-  /**
-   * @description number of categories levels to be returned
-   * @default 2
-   *
-   */
-  levels: number;
-  /**
-   * @description order of categories
-   *
-   *
-   **/
-  categoriesOrder: CategoriesOrder[];
+  levels?: number; // Make levels optional
+  categoriesOrder?: CategoriesOrder[];
 }
 
 export interface CategoriesOrder {
@@ -23,46 +13,35 @@ export interface CategoriesOrder {
   hideCategory?: boolean;
 }
 
-// TODO: Optimize this function later
-
-const categoryTree: LoaderFunction<
-  Props,
-  Categories[] | null,
-  LiveState<{ configVTEX: ConfigVTEX }>
-> = withISFallback(async (_, ctx, { levels, categoriesOrder }) => {
+const categoryTree: LoaderFunction<Props, Categories[] | null, LiveState<{ configVTEX: ConfigVTEX }>> = withISFallback(async (_, ctx, { levels = 2, categoriesOrder = [] }) => {
   const vtex = createClient(ctx.state.global.configVTEX);
-  const categories = await vtex.catalog_system.categoryTree({
-    categoryLevels: levels,
-  });
-  console.log(categoriesOrder);
+  const categories = await vtex.catalog_system.categoryTree({ categoryLevels: levels });
 
-  const categoriesArray = categories as unknown as Array<Categories>;
+  const categoriesArray = categories as unknown as Categories[];
 
-  categoriesArray.forEach((category) => {
+  categoriesArray.map((category) => {
     category.name = category.name.toLowerCase().replace(/\s+/g, "-");
   });
 
-  categoriesOrder.reverse().forEach((category) => {
-    const index = categoriesArray.findIndex(
-      (item) =>
-        item.name.toLowerCase().replace(/\s+/g, "-") ===
-        category.name.toLowerCase().replace(/\s+/g, "-")
-    );
-    if (index !== -1) {
-      categoriesArray[index].hidden = category.hideCategory;
-      const [item] = categoriesArray.splice(index, 1);
-      categoriesArray.unshift(item);
-    }
-  });
-
   categoriesArray.forEach((category) => {
-    category.name = category.name.replace(/-/g, " ");
+    category.name = category.name.toLowerCase().replace(/-/g, " ");
   });
 
-  const finalCategories = categoriesArray.filter(
-    (category) => !category.hidden
-  );
+  const finalCategories = categoriesArray.filter((category) => !category.hidden);
 
+  if (categoriesOrder.length > 0) {
+    const reversedCategoriesOrder = [...categoriesOrder].reverse();
+
+    reversedCategoriesOrder.forEach((category) => {
+      const index = finalCategories.findIndex((item) => item.name.toLowerCase().replace(/\s+/g, "-") === category.name.toLowerCase().replace(/\s+/g, "-"));
+
+      if (index !== -1) {
+        finalCategories[index].hidden = category.hideCategory;
+        const [item] = finalCategories.splice(index, 1);
+        finalCategories.unshift(item);
+      }
+    });
+  }
 
   return {
     data: finalCategories,
