fff06da8baa1a377266dbb174be7876ac1d85ee5
loliveirawebdev <loliveirawebdev@gmail.com>
2023-03-22 20:01:11 -0300

    fix: generate query string from arrays

--- commerce/vnda/utils/queryBuilder.ts
+++ commerce/vnda/utils/queryBuilder.ts
@@ -2,7 +2,11 @@
 export const paramsToQueryString = (params: Record<string, any>): string => {
   const keys = Object.keys(params);
   const validKeys = keys.filter((key) => Boolean(params[key]));
-  const qs = validKeys.map((key) => `${key}=${params[key]}`);
+
+  const qs = validKeys.map((key) => {
+    if (!Array.isArray(params[key])) return `${key}=${params[key]}`;
+    return params[key].map((value: string) => `${key}=${value}`).join("&");
+  });
 
   return qs.join("&");
 };
