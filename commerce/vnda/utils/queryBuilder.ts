fed76a2e4f0c07eb310a3a47f1a2e903b455590f
loliveirawebdev <loliveirawebdev@gmail.com>
2023-03-23 15:10:02 -0300

    fix: use url search params

--- commerce/vnda/utils/queryBuilder.ts
+++ commerce/vnda/utils/queryBuilder.ts
@@ -1,12 +1,7 @@
 // deno-lint-ignore no-explicit-any
-export const paramsToQueryString = (params: Record<string, any>): string => {
-  const keys = Object.keys(params);
-  const validKeys = keys.filter((key) => Boolean(params[key]));
-
-  const qs = validKeys.map((key) => {
-    if (!Array.isArray(params[key])) return `${key}=${params[key]}`;
-    return params[key].map((value: string) => `${key}=${value}`).join("&");
-  });
-
-  return qs.join("&");
+export const paramsToQueryString = (params: Record<string, any>) => {
+  const keys: string[] = Object.keys(params);
+  const validKeys: string[] = keys.filter((key) => Boolean(params[key]));
+  const validParams = validKeys.map((key) => [key, params[key]]);
+  return new URLSearchParams(validParams);
 };
