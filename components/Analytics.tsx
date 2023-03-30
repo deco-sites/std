3e10aca29bf50728941144748137102f3ea8f3c2
igorbrasileiro <brasileiro456@gmail.com>
2023-03-30 09:57:24 -0300

    Add Analytics section

---
+++ components/Analytics.tsx
@@ -0,0 +1,35 @@
+import { context } from "$live/live.ts";
+import GoogleTagManager from "partytown/integrations/GTM.tsx";
+import Script from "partytown/Script.tsx";
+
+export interface Props {
+  /**
+   * @description google tag manager container id. For more info: https://developers.google.com/tag-platform/tag-manager/web#standard_web_page_installation .
+   */
+  trackingIds?: string[];
+}
+
+// const trackingId = "GTM-KVXHNCM";
+
+export default function Analtyics({ trackingIds }: Props) {
+  return (
+    <>
+      {/* TODO: Add debug from query string @author Igor Brasileiro */}
+      {/* Add Tag Manager script during production only. To test it locally remove the condition */}
+      {!context.deploymentId && trackingIds && (
+        trackingIds.map((trackingId) => (
+          <GoogleTagManager trackingId={trackingId.trim()} />
+        ))
+      )}
+
+      <Script
+        dangerouslySetInnerHTML={{
+          // add all globals variables here
+          __html:
+            `debugGlobals = () => { console.table([["datalayer", dataLayer]]); }`,
+        }}
+        forward={["debugGlobals"]}
+      />
+    </>
+  );
+}
