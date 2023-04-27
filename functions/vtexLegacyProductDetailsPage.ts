0ff6a3b015661adbb88eededdf33c57cb99208f4
Marcos Candeia <marrcooos@gmail.com>
2023-04-27 19:03:27 -0300

    Use migrated loader
    Signed-off-by: Marcos Candeia <marrcooos@gmail.com>

--- functions/vtexLegacyProductDetailsPage.ts
+++ functions/vtexLegacyProductDetailsPage.ts
@@ -2,9 +2,9 @@ import type { LoaderFunction } from "$live/types.ts";
 
 import type { ProductDetailsPage } from "../commerce/types.ts";
 import { createClient } from "../commerce/vtex/client.ts";
-import { pickSku, toProductPage } from "../commerce/vtex/transform.ts";
 import type { StateVTEX } from "../commerce/vtex/types.ts";
 import { withSegment } from "../commerce/vtex/withSegment.ts";
+import loader from "../loaders/vtexLegacyProductDetailsPage.ts";
 
 /**
  * @title VTEX Product Page Loader
@@ -21,36 +21,14 @@ const legacyProductPageLoader: LoaderFunction<
   const { global: { configVTEX }, segment } = ctx.state;
   const vtex = createClient(configVTEX);
 
-  const url = new URL(req.url);
-  const skuId = url.searchParams.get("skuId");
-
-  // search products on VTEX. Feel free to change any of these parameters
-  const [product] = await vtex.catalog_system.products.search({
-    term: `${ctx.params.slug}/p`,
-    segment,
-  });
-
-  // Product not found, return the 404 status code
-  if (!product) {
-    return {
-      data: null,
-      status: 404,
-    };
-  }
-
-  const sku = pickSku(product, skuId?.toString());
-
-  const kitItems = sku.isKit
-    ? await vtex.catalog_system.products.search({
-      fq: sku.kitItems!.map((item) => `skuId:${item.itemId}`),
-      segment,
-    })
-    : [];
-
   return {
-    data: toProductPage(product, sku, kitItems, {
-      url,
-      priceCurrency: vtex.currency(),
+    data: await loader({
+      vtexClient: vtex,
+      segment: segment!,
+      slug: ctx.params.slug,
+      reqUrl: req.url,
+    }, {
+      reqUrl: req.url,
     }),
   };
 });
