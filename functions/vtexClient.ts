38cfddce6cc84f198522e3e467a6946fa68820cd
Marcos Candeia <marrcooos@gmail.com>
2023-04-28 16:56:36 -0300

    Migrate loaders (#45)
    * Creating new loaders
    
    Signed-off-by: Marcos Candeia <marrcooos@gmail.com>
    
    * Migrate loaders
    
    Signed-off-by: Marcos Candeia <marrcooos@gmail.com>
    
    * Use migrated loader
    
    Signed-off-by: Marcos Candeia <marrcooos@gmail.com>
    
    * Update live to use branch
    
    Signed-off-by: Marcos Candeia <marrcooos@gmail.com>
    
    * Migrate legacy related products loader
    
    Signed-off-by: Marcos Candeia <marrcooos@gmail.com>
    
    * Bump live rc
    
    Signed-off-by: Marcos Candeia <marrcooos@gmail.com>
    
    * Bump to live v1
    
    Signed-off-by: Marcos Candeia <marrcooos@gmail.com>
    
    ---------
    
    Signed-off-by: Marcos Candeia <marrcooos@gmail.com>

---
+++ functions/vtexClient.ts
@@ -0,0 +1,24 @@
+import type { LoaderFunction } from "$live/types.ts";
+
+import {
+  ClientVTEX,
+  ConfigVTEX,
+  createClient,
+} from "../commerce/vtex/client.ts";
+import { StateVTEX } from "../commerce/vtex/types.ts";
+
+/**
+ * @title Creates a VTEXClient.
+ */
+const vtexClient: LoaderFunction<
+  ConfigVTEX,
+  ClientVTEX,
+  StateVTEX
+> = (
+  _req,
+  ctx,
+) => {
+  return { data: createClient(ctx.state.$live) };
+};
+
+export default vtexClient;
---
+++ loaders/vtexLegacyProductDetailsPage.ts
@@ -0,0 +1,54 @@
+import { LoaderContext } from "$live/types.ts";
+import { ClientVTEX } from "deco-sites/std/commerce/vtex/client.ts";
+import {
+  pickSku,
+  toProductPage,
+} from "deco-sites/std/commerce/vtex/transform.ts";
+import { Slug } from "deco-sites/std/functions/slugFromParams.ts";
+import type { ProductDetailsPage } from "../commerce/types.ts";
+import { ConfigVTEX, createClient } from "../commerce/vtex/client.ts";
+import { Segment } from "../commerce/vtex/types.ts";
+
+export interface Props {
+  slug: Slug;
+  vtexClient: ClientVTEX;
+  segment: Partial<Segment>;
+}
+
+/**
+ * @title VTEX Product Page Loader
+ * @description Works on routes of type /:slug/p
+ */
+export default async function legacyProductPageLoader(
+  { vtexClient: _vtex, segment, slug }: Props,
+  ctx: LoaderContext<{ configVTEX?: ConfigVTEX }>,
+): Promise<ProductDetailsPage | null> {
+  const vtex = _vtex ?? createClient(ctx?.configVTEX);
+  const url = new URL(ctx.reqUrl);
+  const skuId = url.searchParams.get("skuId");
+
+  // search products on VTEX. Feel free to change any of these parameters
+  const [product] = await vtex.catalog_system.products.search({
+    term: `${slug}/p`,
+    segment,
+  });
+
+  // Product not found, return the 404 status code
+  if (!product) {
+    return null;
+  }
+
+  const sku = pickSku(product, skuId?.toString());
+
+  const kitItems = sku.isKit
+    ? await vtex.catalog_system.products.search({
+      fq: sku.kitItems!.map((item) => `skuId:${item.itemId}`),
+      segment,
+    })
+    : [];
+
+  return toProductPage(product, sku, kitItems, {
+    url,
+    priceCurrency: vtex.currency(),
+  });
+}
