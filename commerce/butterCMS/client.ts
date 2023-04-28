b341ce0b810e6a968890c9e116f93ce81e058f40
gustavo-vasconcellos <gustavo.vasconcellos@hp.com>
2023-04-28 19:10:56 -0300

    feat(butterCMS): create client and typings

---
+++ commerce/butterCMS/client.ts
@@ -0,0 +1,89 @@
+import { Account } from "$live/blocks/account.ts";
+import { fetchAPI } from "../../utils/fetchAPI.ts";
+import { CategoriesData, Page, PostsData } from "./types.ts";
+
+export interface ConfigButterCMS extends Account {
+  authToken: string;
+  /**
+   * @description Default value: v2
+   */
+  apiVersion: string;
+  defaultLocale: string;
+}
+
+export type ClientOCC = ReturnType<typeof createClient>;
+
+export const createClient = (
+  { authToken, apiVersion = "v2", defaultLocale }: ConfigButterCMS,
+) => {
+  const baseUrl = `https://api.buttercms.com/${apiVersion}`;
+
+  const categories = () => {
+    const params = new URLSearchParams({
+      authToken,
+    });
+
+    const url = new URL(`/categories?${params.toString()}`, baseUrl);
+
+    return fetchAPI<CategoriesData>(url);
+  };
+
+  const post = (slug: string) => {
+    const params = new URLSearchParams({
+      authToken,
+    });
+
+    const url = new URL(
+      `/posts/${slug}?auth_token=?${params.toString()}`,
+      baseUrl,
+    );
+
+    return fetchAPI<PostsData>(url);
+  };
+
+  const posts = (
+    page: number,
+    pageSize: number,
+    excludeBody = true,
+    tagSlug?: string,
+  ) => {
+    const params = new URLSearchParams({
+      authToken,
+      page: page.toString(),
+      pageSize: pageSize.toString(),
+      excludeBody: excludeBody.toString(),
+      tagSlug: tagSlug ?? "",
+    });
+
+    const url = new URL(
+      `/posts?auth_token=?${params.toString()}`,
+      baseUrl,
+    );
+
+    return fetchAPI<PostsData>(url);
+  };
+
+  const pages = (locale?: string) => {
+    const params = new URLSearchParams({
+      authToken,
+      /**
+       * @todo after add locales logic there is no use of defaultLocale
+       */
+      locale: locale ?? defaultLocale,
+    });
+
+    const url = new URL(
+      `/pages/*/blog-v2?auth_token=?${params.toString()}`,
+      baseUrl,
+    );
+
+    return fetchAPI<Page>(url);
+  };
+
+  return {
+    categories,
+    posts,
+    post,
+    pages,
+  };
+};
---
+++ commerce/butterCMS/types.ts
@@ -0,0 +1,177 @@
+export interface Page {
+  data: PageData;
+}
+
+export interface PageData {
+  slug: string;
+  name: string;
+  published: string;
+  updated: string;
+  page_type: string;
+  fields: DataFields;
+}
+
+export interface DataFields {
+  sections: Section[];
+}
+
+export type Section =
+  | PostsSection
+  | PlacesSection
+  | BrandSection
+  | AdSection
+  | ArticleSection;
+
+export interface PostsSection {
+  type: "featured_posts";
+  fields: PostFields;
+}
+export interface PostFields {
+  title: string;
+  previews: PreviewField[];
+}
+
+export interface PlacesSection {
+  type: "featured_places";
+  fields: PlacesFields;
+}
+export interface PlacesFields {
+  title: string;
+  places: PlaceField[];
+}
+
+export interface BrandSection {
+  type: "featured_brands";
+  fields: BrandFields;
+}
+export interface BrandFields {
+  title: string;
+  brands: BrandField[];
+}
+
+export interface AdSection {
+  type: "featured_ads";
+  fields: AdFields;
+}
+export interface AdFields {
+  title: string;
+  ads: AdField[];
+}
+
+export interface ArticleSection {
+  type: "featured_articles";
+  fields: ArticleFields;
+}
+export interface ArticleFields {
+  title: string;
+  articles?: ArticleField[];
+}
+
+export interface ArticleField {
+  article: Place;
+}
+
+export interface AdField {
+  ad: Ad;
+}
+
+export interface Ad {
+  meta: Meta;
+  title: string;
+  summary: string;
+  category: string;
+  image: string;
+  image_alt: string;
+  url_text: string;
+  url_slug: string;
+}
+
+export interface BrandField {
+  brand: Place;
+}
+
+export interface PlaceField {
+  place: Place;
+}
+
+export interface Place {
+  meta: Meta;
+  name: string;
+  catalogue_slug: string;
+}
+
+export interface PreviewField {
+  preview: Preview;
+}
+
+export interface Preview {
+  meta: Meta;
+  title: string;
+  summary: string;
+  featured_image: string;
+  featured_image_alt: string;
+  category: string;
+  slug: string;
+}
+
+export interface Meta {
+  id: number;
+}
+
+export interface CategoriesData {
+  data: Category[];
+}
+
+export interface Category {
+  name: string;
+  slug: string;
+}
+
+export interface PostsData {
+  meta: Meta;
+  data: Post[];
+}
+
+export interface Post {
+  status: string;
+  created: string;
+  updated: string;
+  published: string;
+  title: string;
+  slug: string;
+  summary: string;
+  seo_title: string;
+  meta_description: string;
+  featured_image_alt: string;
+  url: string;
+  featured_image: string;
+  author: Author;
+  tags: Tag[];
+  categories: Tag[];
+}
+
+export interface Tag {
+  name: string;
+  slug: string;
+}
+
+export interface Author {
+  bio: string;
+  slug: string;
+  email: string;
+  title: string;
+  last_name: string;
+  first_name: string;
+  facebook_url: string;
+  linkedin_url: string;
+  instagram_url: string;
+  pinterest_url: string;
+  profile_image: string;
+  twitter_handle: string;
+}
+
+export interface Meta {
+  next_page: number;
+  previous_page?: number;
+  count: number;
+}
