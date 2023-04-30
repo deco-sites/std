8b6dce1a3b91126aa6aedd0923e65276165314ce
Marcos Candeia <marrcooos@gmail.com>
2023-04-30 09:24:47 -0300

    Fix related products loader
    Signed-off-by: Marcos Candeia <marrcooos@gmail.com>

--- functions/vtexLegacyRelatedProductsLoader.ts
+++ functions/vtexLegacyRelatedProductsLoader.ts
@@ -1,11 +1,9 @@
 import type { LoaderFunction } from "$live/types.ts";
 
-import { withSegment } from "../commerce/vtex/withSegment.ts";
-import { toProduct } from "../commerce/vtex/transform.ts";
-import { createClient } from "../commerce/vtex/client.ts";
-import type { CrossSellingType } from "../commerce/vtex/types.ts";
 import type { Product } from "../commerce/types.ts";
-import type { StateVTEX } from "../commerce/vtex/types.ts";
+import { createClient } from "../commerce/vtex/client.ts";
+import type { CrossSellingType, StateVTEX } from "../commerce/vtex/types.ts";
+import { withSegment } from "../commerce/vtex/withSegment.ts";
 import loader from "../loaders/vtexLegacyRelatedProductsLoader.ts";
 export interface Props {
   /**
@@ -34,35 +32,16 @@ const legacyRelatedProductsLoader: LoaderFunction<
 ) => {
   const { global: { configVTEX } } = ctx.state;
   const vtex = createClient(configVTEX);
-  const url = new URL(req.url);
-
-  const pageType = await vtex.catalog_system.portal.pageType({
-    slug: `${ctx.params.slug}/p`,
-  });
-
-  // Page type doesn't exists or this is not product page
-  if (!pageType || pageType.pageType !== "Product" || !pageType.id) {
-    return {
-      data: null,
-    };
-  }
-
-  if (!crossSelling) {
-    return {
-      data: null,
-    };
-  }
-
-  const vtexRelatedProducts = await vtex.catalog_system.products.crossSelling(
-    { productId: pageType.id, type: crossSelling },
-  );
-
-  const relatedProducts = vtexRelatedProducts.slice(0, count ?? Infinity).map((
-    p,
-  ) => toProduct(p, p.items[0], 0, { url, priceCurrency: vtex.currency() }));
 
   return {
-    data: relatedProducts,
+    data: await loader({
+      vtexClient: vtex,
+      slug: ctx.params.slug,
+      crossSelling,
+      count,
+    }, {
+      reqUrl: req.url,
+    }),
   };
 });
 
