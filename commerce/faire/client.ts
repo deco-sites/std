84cc63d1507dd40762c1245a4ce5828f64831075
Lucis <luciannojunior@gmail.com>
2023-05-06 16:36:34 -0300

    Using other URL to avoid CF Scrape Shield

--- commerce/faire/client.ts
+++ commerce/faire/client.ts
@@ -3,14 +3,9 @@ import { FaireSearchResult } from "./types.ts";
 
 export default {
   searchProducts: async (query: string) => {
-    const headers = new Headers();
-    headers.set("content-type", "application/json");
-    headers.set("Origin", "https://www.faire.com/");
-    headers.set("user-agent", "deco.cx");
-
     // TODO: Create Account and use it
     const result = (await fetchAPI(
-      `https://proxy-faire.deco-cx.workers.dev?query=${query}`,
+      `https://lucisteste.mesa.land?query=${query}`,
     ).catch(console.log)) as FaireSearchResult;
 
     return result;
