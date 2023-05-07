3aa26c0f8e41cc159a08d303690aac87356addf6
Lucis <luciannojunior@gmail.com>
2023-05-06 23:50:44 -0300

    Using proxy

--- commerce/faire/client.ts
+++ commerce/faire/client.ts
@@ -10,40 +10,7 @@ export default {
 
     // TODO: Create Account and use it
     const result = (await fetchAPI(
-      "https://www.faire.com/api/v2/products/search/query-products-with-lean-tiles",
-      {
-        withProxyCache: true,
-        headers,
-        method: "POST",
-        body: JSON.stringify({
-          "maker_values": [],
-          "filter_keys": [],
-          "range_filters": [],
-          "previous_filter_keys": [],
-          "initial_request_exposed_visual_filter_option_keys": [],
-          query,
-          "page_number": 0,
-          "page_size": 60,
-          "return_filter_sections": true,
-          "container_name": "search_results_grid",
-          "referrer": {
-            "referrer_type": "NONE",
-          },
-          "allow_query_pruning": true,
-          "allow_related_brands_search": true,
-          "allow_semantically_similar_results": true,
-          "do_query_spell_check": true,
-          "return_related_explore_keyword": false,
-          "filter_split_strategy": "PRODUCTS_ONLY",
-          "brand_off_faire_search_method":
-            "BRAND_OFF_FAIRE_QUERY_METHOD_UNKNOWN",
-          "enable_brand_off_faire_query_response": true,
-          "return_preorderable_products": true,
-          "enable_preorderable_delivery_window_filter": true,
-          "return_retrieval_and_ranking_debug_info": false,
-          "product_search_method": "PRODUCT_SEARCH_METHOD_UNKNOWN",
-        }),
-      },
+      `https://proxy-faire.deco-cx.workers.dev?query=${query}`,
     ).catch(console.log)) as FaireSearchResult;
 
     return result;
