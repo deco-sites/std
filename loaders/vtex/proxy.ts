5bb7a03871af343380ab1884f69f9dec72082db9
Lucis <luciannojunior@gmail.com>
2023-06-03 17:04:56 -0300

    Using configVTEX for proxy

--- loaders/vtex/proxy.ts
+++ loaders/vtex/proxy.ts
@@ -1,14 +1,7 @@
+import type { Context } from "deco-sites/std/packs/vtex/accounts/vtex.ts";
 import { Route } from "$live/flags/audience.ts";
 
-export interface Props {
-  /**
-   * A valid VTEX Store domain.
-   * @example fashion.bravtexstores.com.br
-   */
-  hostname: string;
-}
-
-const pathsToProxy = [
+const PATHS_TO_PROXY = [
   "/sitemap.xml",
   "/sitemap/*",
   "/checkout",
@@ -23,17 +16,51 @@ const pathsToProxy = [
   "/_secure/account",
 ];
 
-const buildProxyRoutes = (url: string) =>
-  pathsToProxy.map((pathTemplate) => ({
-    pathTemplate,
-    handler: { value: { url, __resolveType: "$live/handlers/proxy.ts" } },
-  }));
+const buildProxyRoutes = ({ publicUrl }: { publicUrl?: string }) => {
+  if (!publicUrl) {
+    return [];
+  }
+
+  try {
+    const hostname = (new URL(
+      publicUrl?.startsWith("http") ? publicUrl : `https://${publicUrl}`,
+    )).hostname;
+
+    // Rejects TLD mystore.com, which VTEX doesn't support it carrefour.com.br / CNAME
+    if (!hostname || hostname.split(".").length <= 2) {
+      throw new Error(`Invalid hostname from '${publicUrl}'`);
+    }
+
+    // TODO @lucis: Fix the proxy, MITM
+
+    // const urlToProxy = `https://${hostname}.cdn.vtex.com`;
+    const urlToProxy = `https://${hostname}`;
+    const hostToUse = hostname;
+
+    return PATHS_TO_PROXY.map((pathTemplate) => ({
+      pathTemplate,
+      handler: {
+        value: {
+          __resolveType: "$live/handlers/proxy.ts",
+          url: urlToProxy,
+          host: hostToUse,
+        },
+      },
+    }));
+  } catch (e) {
+    console.log("Error parsing publicUrl from configVTEX");
+    console.error(e);
+    return [];
+  }
+};
 
 /**
  * @title VTEX Proxy Routes
  */
 export default function VTEXProxy(
-  { hostname }: Props,
+  _props: unknown,
+  _req: Request,
+  ctx: Context,
 ): Route[] {
-  return buildProxyRoutes(`https://${hostname}`);
+  return buildProxyRoutes({ publicUrl: ctx.configVTEX?.publicUrl });
 }
