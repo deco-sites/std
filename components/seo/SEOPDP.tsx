ff1cacec5be1a38e18512f9f673d0535831892a9
Lucis <luciannojunior@gmail.com>
2023-06-22 17:32:36 -0300

    Also supporting last category

--- components/seo/SEOPDP.tsx
+++ components/seo/SEOPDP.tsx
@@ -1,5 +1,6 @@
 import Metatags from "./Metatags.tsx";
 import type { LoaderReturnType } from "$live/types.ts";
+import { DEFAULT_CATEGORY_SEPARATOR } from "deco-sites/std/commerce/utils.ts";
 import type { ProductDetailsPage } from "../../commerce/types.ts";
 
 export interface Props {
@@ -16,23 +17,32 @@ export interface Props {
   page: LoaderReturnType<ProductDetailsPage | null>;
   structuredData?: {
     useDataFromSEO?: boolean;
+    useOnlyLastCategory?: boolean;
   };
 }
 
 const SeoPDP = (props: Props) => {
   const context = (() => {
-    if (props.structuredData?.useDataFromSEO) {
-      return {
-        ...props.page,
-        product: {
-          ...props.page?.product,
-          name: props.page?.seo?.title,
-          description: props.page?.seo?.description,
-        },
-      } as ProductDetailsPage;
-    } else {
+    if (!props.structuredData?.useDataFromSEO || !props.page?.product) {
       return props.page;
     }
+
+    const product = props.page?.product;
+    // For "Calçados>Masculino>Chinelos & Sandálias", only returns "Chinelos & Sandálias"
+    const lastCategory =
+      product.category?.split(DEFAULT_CATEGORY_SEPARATOR).reverse()[0];
+
+    return {
+      ...props.page,
+      product: {
+        ...product,
+        name: props.page?.seo?.title,
+        description: props.page?.seo?.description,
+        category: props.structuredData?.useOnlyLastCategory
+          ? lastCategory
+          : product.category,
+      },
+    } as ProductDetailsPage;
   })();
 
   return <Metatags {...props} context={context} />;
