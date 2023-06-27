acfb6de9f456b9f610a2c3f712ef5ce21fa4bf19
gimenes <tlgimenes@gmail.com>
2023-06-27 15:12:13 -0300

    add sitemap handler

--- handlers/vtex/sitemap.ts
+++ handlers/vtex/sitemap.ts
@@ -1,2 +1,1 @@
 export { default } from "deco-sites/std/packs/vtex/handlers/sitemap.ts";
-export type { Props } from "deco-sites/std/packs/vtex/handlers/sitemap.ts";
--- packs/vtex/handlers/sitemap.ts
+++ packs/vtex/handlers/sitemap.ts
@@ -1,26 +1,37 @@
 import Proxy from "$live/handlers/proxy.ts";
-import { Handler } from "std/http/mod.ts";
-
-export interface Props {
-  /**
-   * @description the proxy sitemap url.
-   * @example https://bravtexfashionstore.vtexcommercestable.com.br
-   */
-  url: string;
-}
+import { ConnInfo } from "std/http/server.ts";
+import { isFreshCtx } from "$live/handlers/fresh.ts";
+import type { Account } from "deco-sites/std/packs/vtex/accounts/vtex.ts";
 
 /**
- * @title Proxy Handler
- * @description Proxies request to the target url.
+ * @title Sitemap Proxy
  */
-export default function Sitemap({ url }: Props): Handler {
-  return async (req, ctx) => {
-    const response = await Proxy({ url })(req, ctx);
+export default function Sitemap(_: unknown) {
+  return async (
+    req: Request,
+    ctx: ConnInfo,
+  ) => {
+    if (!isFreshCtx<{ configVTEX: Account }>(ctx)) {
+      throw new Error("Missing fresh context");
+    }
+
+    const url = ctx.state.configVTEX?.publicUrl;
+
+    if (!url) {
+      throw new Error("Missing configVTEX.publicUrl");
+    }
+
+    const publicUrl =
+      new URL(url?.startsWith("http") ? url : `https://${url}`).href;
+
+    const response = await Proxy({
+      url: publicUrl,
+    })(req, ctx);
 
     const reqUrl = new URL(req.url);
     const text = await response.text();
 
-    return new Response(text.replaceAll(url, reqUrl.origin), {
+    return new Response(text.replaceAll(publicUrl, `${reqUrl.origin}/`), {
       headers: response.headers,
       status: response.status,
     });
