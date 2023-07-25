41a452cee27c3a418fa6d74ff01d0b640108f4c6
Tiago Gimenes <tlgimenes@gmail.com>
2023-07-25 16:52:39 -0300

    add paths to vnda proxy (#161)

--- tailwindv3.ts
+++ tailwindv3.ts
@@ -3,7 +3,7 @@ import { join, toFileUrl } from "std/path/mod.ts";
 import { cyan } from "std/fmt/colors.ts";
 import postcss, { PluginCreator } from "npm:postcss@8.4.23";
 import autoprefixer from "npm:autoprefixer@10.4.14";
-import tailwindcss, { Config as TailwindConfig } from "npm:tailwindcss@3.3.2";
+import tailwindcss, { Config as TailwindConfig } from "npm:tailwindcss@3.3.3";
 import cssnano from "npm:cssnano@6.0.0";
 import { FROM, tailwindBundle, TO } from "./routes/styles.css.ts";
 
