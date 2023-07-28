a1f2138678692470218004bc1b7c0d1662d4cb29
gimenes <tlgimenes@gmail.com>
2023-07-27 21:26:57 -0300

    create tailwind plugin

--- dev.ts
+++ dev.ts
@@ -1,4 +1,12 @@
 #!/usr/bin/env -S deno run -A --watch=static/
+
+/**
+ * TODO: I know, this makes no sense. Try to remove it and see what happens.
+ * If server boots, remove this line. This might be a deno bug solved in future
+ * releases (>1.35.3)
+ */
+import "./plugins/tailwind/bundler.ts";
+
 import dev from "$live/dev.ts";
 import liveManifest from "$live/live.gen.ts";
 
--- routes/styles.css.ts
+++ routes/styles.css.ts
@@ -1,29 +1,29 @@
-// import type { Handlers } from "$fresh/server.ts";
-// import { yellow } from "std/fmt/colors.ts";
-// import { handler as tailwindHandler } from "../plugins/tailwind/mod.ts";
+import type { Handlers } from "$fresh/server.ts";
+import { yellow } from "std/fmt/colors.ts";
+import { handler as tailwindHandler } from "../plugins/tailwind/mod.ts";
 
-// const msg = `
-// ${yellow("WARNING!")}
-// Tailwind setup has changed and we now offer it via a Fresh Plugin! This file will be removed in future releases. To migrate
+const msg = `
+${yellow("WARNING!")}
+Tailwind setup has changed and we now offer it via a Fresh Plugin! This file will be removed in future releases. To migrate
 
-// 1. Remove routes/styles.css.ts from your routes folder
-// 2. Change main.ts to:
+1. Remove routes/styles.css.ts from your routes folder
+2. Change main.ts to:
 
-//   import tailwindPlugin from "deco-sites/std/plugins/tailwind/mod.ts";
+  import tailwindPlugin from "deco-sites/std/plugins/tailwind/mod.ts";
 
-//   await start($live(manifest, site), {
-//     plugins: [
-//       tailwindPlugin,
-//     ],
-//   });
+  await start($live(manifest, site), {
+    plugins: [
+      tailwindPlugin,
+    ],
+  });
 
-// That's it! Thanks for migrating 🎉
-// `;
+That's it! Thanks for migrating 🎉
+`;
 
-// export const handler: Handlers = {
-//   GET: (req, ctx) => {
-//     console.warn(msg);
+export const handler: Handlers = {
+  GET: (req, ctx) => {
+    console.warn(msg);
 
-//     return tailwindHandler.GET!(req, ctx);
-//   },
-// };
+    return tailwindHandler.GET!(req, ctx);
+  },
+};
