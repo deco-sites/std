b089d23dc85c31acd9ee25ce228246128aca44a4
Lucis <luciannojunior@gmail.com>
2023-07-30 17:33:57 -0300

    Not including Quill CSS in HTMLRenderer (#170)
    * Not including Quill CSS
    
    * Including class

--- components/HTMLRenderer.tsx
+++ components/HTMLRenderer.tsx
@@ -1,6 +1,26 @@
-/**
- * I'm creating this component so we don't tie ourselves
- * with Quill
- */
-export type { HTML } from "./types.ts";
-export { default as default } from "./QuillText.tsx";
+import type { ComponentType, JSX } from "preact";
+import type { HTML } from "./types.ts";
+export type { HTML };
+
+export interface Props {
+  html: HTML;
+  component?: string;
+  as?: keyof JSX.IntrinsicElements | ComponentType;
+  class?: string;
+}
+
+export default function HTMLRenderer(
+  { as = "div", class: _class, html }: Props,
+) {
+  const Component = as as ComponentType<{
+    class?: string;
+    dangerouslySetInnerHTML: { __html: string };
+  }>;
+
+  return (
+    <Component
+      class={_class}
+      dangerouslySetInnerHTML={{ __html: html }}
+    />
+  );
+}
