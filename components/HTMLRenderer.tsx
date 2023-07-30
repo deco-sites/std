a24758fbbb9143eb2a1e560ee5cd54ff06f7d72e
Lucis <luciannojunior@gmail.com>
2023-07-30 17:32:04 -0300

    Not including Quill CSS

--- components/HTMLRenderer.tsx
+++ components/HTMLRenderer.tsx
@@ -1,6 +1,18 @@
-/**
- * I'm creating this component so we don't tie ourselves
- * with Quill
- */
-export type { HTML } from "./types.ts";
-export { default as default } from "./QuillText.tsx";
+import type { ComponentType, JSX } from "preact";
+import type { HTML } from "./types.ts";
+export type { HTML };
+
+export interface Props {
+  html: HTML;
+  component?: string;
+  as?: keyof JSX.IntrinsicElements | ComponentType;
+  class?: string;
+}
+
+export default function HTMLRenderer({ as = "div", html }: Props) {
+  const Component = as as ComponentType<{
+    dangerouslySetInnerHTML: { __html: string };
+  }>;
+
+  return <Component dangerouslySetInnerHTML={{ __html: html }} />;
+}
