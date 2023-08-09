7e97c1df8c51889662d6710328983b8bb6ada2eb
Marcos Candeia <marrcooos@gmail.com>
2023-08-09 13:35:34 -0300

    Use deco plugin (#186)
    * Use deco plugin
    
    Signed-off-by: Marcos Candeia <marrcooos@gmail.com>
    
    * Add warning deprecation message
    
    Signed-off-by: Marcos Candeia <marrcooos@gmail.com>
    
    * Set inject routes to false
    
    Signed-off-by: Marcos Candeia <marrcooos@gmail.com>
    
    ---------
    
    Signed-off-by: Marcos Candeia <marrcooos@gmail.com>

--- plugins/mod.ts
+++ plugins/mod.ts
@@ -1,7 +1,30 @@
 import type { Plugin } from "$fresh/server.ts";
+import decoPlugin from "$live/plugins/deco.ts";
 import { plugin as tailwindPlugin } from "./tailwind/mod.ts";
+import * as colors from "std/fmt/colors.ts";
 
-export const plugin = (): Plugin => ({
-  ...tailwindPlugin,
-  name: "deco-sites/std",
-});
+export const plugin = (): Plugin => {
+  console.warn(
+    colors.brightYellow(
+      `deco-sites/std plugin has been deprecated, use default export instead. You must change your dev.ts and main.ts, check out the following examples\ndev.ts: ${
+        colors.brightGreen(
+          "https://github.com/deco-sites/std/blob/main/dev.ts#L14",
+        )
+      }\nmain.ts: ${
+        colors.brightGreen(
+          "https://github.com/deco-sites/std/blob/main/main.ts#L15",
+        )
+      }`,
+    ),
+  );
+  return ({
+    ...tailwindPlugin,
+    name: "deco-sites/std",
+  });
+};
+
+const plugins = (): Plugin[] => {
+  return [tailwindPlugin, decoPlugin()];
+};
+
+export default plugins;
