678353ffff168f70aba8ec7032198e940462a5a4
Marcos Candeia <marrcooos@gmail.com>
2023-09-06 13:03:04 -0300

    Transform sitemap context (#222)
    Signed-off-by: Marcos Candeia <marrcooos@gmail.com>

--- packs/vtex/handlers/sitemap.ts
+++ packs/vtex/handlers/sitemap.ts
@@ -1,6 +1,13 @@
-import base from "https://denopkg.com/deco-cx/apps@0.3.5/vtex/handlers/sitemap.ts";
+import type { Context } from "deco-sites/std/packs/vtex/accounts/vtex.ts";
+import { transform } from "deco-sites/std/packs/vtex/utils/future.ts";
+import {
+  default as base,
+  Props,
+} from "https://denopkg.com/deco-cx/apps@0.3.5/vtex/handlers/sitemap.ts";
 
 /**
  * @title Sitemap Proxy
  */
-export default base;
+export default function SiteMapProxy(props: Props, ctx: Context) {
+  return base(props, transform(ctx));
+}
