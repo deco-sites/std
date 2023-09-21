e6b27b416824763aec1ce305a2000a75c6122d7d
gimenes <tlgimenes@gmail.com>
2023-09-21 11:41:51 -0300

    bump apps

--- packs/vtex/loaders/intelligentSearch/productList.ts
+++ packs/vtex/loaders/intelligentSearch/productList.ts
@@ -3,10 +3,10 @@ import type { Context } from "deco-sites/std/packs/vtex/accounts/vtex.ts";
 import { transform } from "deco-sites/std/packs/vtex/utils/future.ts";
 import base, {
   Props,
-} from "https://denopkg.com/deco-cx/apps@0.7.4/vtex/loaders/intelligentSearch/productList.ts";
+} from "https://denopkg.com/deco-cx/apps@0.9.0/vtex/loaders/intelligentSearch/productList.ts";
 export type {
   Props,
-} from "https://denopkg.com/deco-cx/apps@0.7.4/vtex/loaders/intelligentSearch/productList.ts";
+} from "https://denopkg.com/deco-cx/apps@0.9.0/vtex/loaders/intelligentSearch/productList.ts";
 
 /**
  * @title VTEX Intelligent Search - Search Products
